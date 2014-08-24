450fb155854ef0f33056e3dbea724556e39a8117
Jefferson Leard <jtleard@fandor.com>
2014-08-23 18:32:11 -0700

    Add basic enemy class to inherit from.

---
+++ source/Enemy.hx
@@ -0,0 +1,17 @@
+package;
+
+import flixel.FlxSprite;
+
+class Enemy extends FlxSprite {
+	override public function new():Void {
+		super();
+	}
+
+	override public function update():Void {
+		super.update();
+	}
+
+	override public function destroy():Void {
+		super.destroy();
+	}
+}
