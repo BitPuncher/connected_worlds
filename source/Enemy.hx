60eaca1c90fe5e400be440f16fadbbdee64097da
Michael Hernandez <lordcrowman@gmail.com>
2014-08-23 23:38:33 -0500

    Starting enemy stuff

--- source/Enemy.hx
+++ source/Enemy.hx
@@ -1,10 +1,19 @@
 package;
 
 import flixel.FlxSprite;
+import flixel.util.FlxTimer;
 
-class Enemy extends FlxSprite {
+class Enemy extends FlxSprite 
+{
+	private var behaviorTimer:FlxTimer;
+	private var time:Int;
+
 	override public function new():Void {
 		super();
+
+		time = 10;
+
+		behaviorTimer = new FlxTimer(time, behavior, 0)
 	}
 
 	override public function update():Void {
@@ -13,5 +22,10 @@ class Enemy extends FlxSprite {
 
 	override public function destroy():Void {
 		super.destroy();
+	}
+
+	public function behavior(timer:FlxTimer):Void
+	{
+		
 	}
 }
